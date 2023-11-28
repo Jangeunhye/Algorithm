b1c711b901b5c5c8122e1ebbebd0bf3fd312290e
Jangeunhye <65762430+Jangeunhye@users.noreply.github.com>
2023-11-28 21:29:06 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,9 +1,4 @@
 s = input()
-array = [-1]*26
-for i in s:
-  for j in range(26):
-    if(i == chr(j+97) and array[j]!=1):
-      array[j]= s.find(i)
-
-for i in array:
-  print(i, end=" ")
+c = 'abcdefghijklmnopqrstuvwxyz'
+for i in c:
+  print(s.find(i),end=" ")
