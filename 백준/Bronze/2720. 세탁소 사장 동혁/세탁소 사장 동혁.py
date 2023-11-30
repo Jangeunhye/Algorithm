a1862ad9d8e459b0f1aa53644423696fb3baacc1
Jangeunhye <65762430+Jangeunhye@users.noreply.github.com>
2023-11-30 22:35:32 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -1,20 +1,10 @@
 q, d,n,p = 0,0,0,0
 t = int(input())
 for i in range(t):
-  c = int(input())
+    c = int(input()) 
+    array = [25,10,5,1]
+    for j in array:
+        print(c//j,end=" ")
+        c = c%j
+    print()
 
-  #쿼터
-  q =c//25
-  c = c%25
-
-  #다임
-  d = c//10
-  c= c%10
-
-  #니켈
-  n = c//5
-  c = c%5
-  
-  #페니
-  p = c//1
-  print(q,d,n,p)
