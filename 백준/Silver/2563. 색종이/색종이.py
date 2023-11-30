4ab105bfceeddeddaefe42b0f1885c28fbd3caea
Jangeunhye <65762430+Jangeunhye@users.noreply.github.com>
2023-11-30 22:16:23 +0900

    [Silver V] Title: 색종이, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,16 +1,13 @@
 n = int(input())
-array= [[0]*100 for _ in range(100)]
-
+array = [[0 for _ in range(101)] for _ in range(101)]
 for _ in range(n):
-  x, y = map(int, input().split())
-  for i in range(99-y,89-y,-1):
-        for j in range(x, x+10):
-            array[i][j] +=1
-
+  x, y  = map(int, input().split())
+  for i in range(y,y+10):
+    for j in range(x, x+10):
+      array[i][j]  = 1
 sum = 0
-for i in range(100):
-    for j in range(100):
-        if (array[i][j]>=2):
-            sum += (array[i][j]-1)
+for i in range(1,101):
+  for j in range(1, 101):
+    sum += array[i][j]
 
-print(100*n-sum)
+print(sum) 
