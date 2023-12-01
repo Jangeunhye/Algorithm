6abc27cc91f5813778243ddea3e327f736f3d2ea
Jangeunhye <65762430+Jangeunhye@users.noreply.github.com>
2023-12-01 13:55:28 +0900

    [level 1] Title: 과일 장수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,11 +1,2 @@
 def solution(k, m, score):
-    answer = 0
-    array = []
-    score = sorted(score)
-    for i in range(len(score)//m):
-        array.append([])
-        for j in range(m):
-            array[i].append(score.pop())
-        answer += min(array[i])*m
-    
-    return answer
+    return(sum(sorted(score)[len(score)%m::m])*m)
