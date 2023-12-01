5892b3a1edb5af18e9d81f26e4ea6d8ca9e1b834
Jangeunhye <65762430+Jangeunhye@users.noreply.github.com>
2023-12-01 19:50:07 +0900

    [level 1] Title: 폰켓몬, Time: 0.20 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,11 +1,2 @@
 def solution(nums):
-    temp = set(nums)
-    answer=0
-    
-    for i in range(len(nums)//2):
-        temp.pop()
-        answer+=1
-        if(len(temp)<=0):
-            break
-        
-    return answer
+    return min(len(nums)/2,len(set(nums)))
