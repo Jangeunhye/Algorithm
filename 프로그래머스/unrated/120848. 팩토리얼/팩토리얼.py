cb51b1bfe779bdfe7d6056109c18cc719adb8f8b
Jangeunhye <65762430+Jangeunhye@users.noreply.github.com>
2023-12-02 23:42:05 +0900

    [level unrated] Title: 팩토리얼, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120848. 팩토리얼/팩토리얼.py
+++ 프로그래머스/unrated/120848. 팩토리얼/팩토리얼.py
@@ -1,10 +1,7 @@
 def solution(n):
     i=2
-    while True:
-        if(n//i>0):
-            n=n//i
-            i+=1
-        else:
-            break
+    while n//i > 0:
+        n=n//i
+        i+=1
     return i-1
             
