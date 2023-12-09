50b1d34c5569c7f6a69ba158560ca0a887e8303e
Jangeunhye <65762430+Jangeunhye@users.noreply.github.com>
2023-12-10 00:15:07 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 44.21 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -2,20 +2,13 @@ def solution(participant, completion):
     comp_dict = dict()
     part_dict = dict()
     for i in completion:
-        if comp_dict.get(i)!=None:
-             comp_dict[i] +=1
-        else:
-            comp_dict[i]=1
+        comp_dict[i]=1 if comp_dict.get(i)==None else comp_dict[i]+1
         
     for j in participant:
-        if part_dict.get(j)!=None:
-             part_dict[j] +=1
-        else:
-            part_dict[j]=1
+        part_dict[j]=1 if part_dict.get(j)==None else part_dict[j]+1
         
         
     for k in part_dict.keys():
-
-        if comp_dict.get(k)==None or part_dict[k]!=comp_dict[k]:
+        if comp_dict.get(k)!=part_dict.get(k):
             return k
 
