1ee724a2a3a3db07f434c8f4f3d38f83e6c55ebe
Jangeunhye <65762430+Jangeunhye@users.noreply.github.com>
2023-12-10 00:04:54 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 45.51 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,13 +1,21 @@
 def solution(participant, completion):
-    part_hash = dict()
-    hashSum = 0
+    comp_dict = dict()
+    part_dict = dict()
+    for i in completion:
+        if comp_dict.get(i)!=None:
+             comp_dict[i] +=1
+        else:
+            comp_dict[i]=1
+        
+    for j in participant:
+        if part_dict.get(j)!=None:
+             part_dict[j] +=1
+        else:
+            part_dict[j]=1
+        
+        
+    for k in part_dict.keys():
 
-    # 1. participant해시 값 구하기
-    for i in range(len(participant)):
-        part_hash[hash(participant[i])] = participant[i]
-        hashSum +=hash(participant[i])
-    # 2. completion 해시값 구하기.
-    for j in range(len(completion)):
-        hashSum -= hash(completion[j])
-    
-    return part_hash[hashSum]
+        if comp_dict.get(k)==None or part_dict[k]!=comp_dict[k]:
+            return k
+
