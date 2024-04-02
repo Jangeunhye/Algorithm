ea6b77cfae31899356b37e0f48993cc94c4e846e
Jangeunhye <65762430+Jangeunhye@users.noreply.github.com>
2024-04-02 18:15:49 +0900

    [Bronze II] Title: 진법 변환, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.py
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -2,15 +2,9 @@ N, B = input().split()
 B = int(B)
 sum = 0
 
-if(N.isalnum()):
-    for i in range(len(N)):
-        if(N[i].isdigit()):
-            # 숫자일때
-            sum +=(B**(len(N)-i-1))*int(N[i])
-            
-        elif(N[i].isalpha()):
-            # 숫자가 아닐때
-            temp = ord(N[i])-55
-            sum +=(B**(len(N)-i-1))*temp
-
+for i in range(len(N)):
+    if(N[i].isdigit()):
+        sum += B**(len(N)-i-1)*int(N[i])
+    else:
+        sum +=(B**(len(N)-i-1) )* (ord(N[i])-55)
 print(sum)
