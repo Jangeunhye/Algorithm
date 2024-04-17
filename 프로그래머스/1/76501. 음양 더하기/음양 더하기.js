67462cdd22b241460a5da0e9655dd5577e5ce3fb
Jangeunhye <65762430+Jangeunhye@users.noreply.github.com>
2024-04-17 11:22:39 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,7 +1,3 @@
 function solution(absolutes, signs) {
-    let result = 0;
-    for(let i = 0; i<absolutes.length;i++){
-        result += signs[i]?absolutes[i]:(-absolutes[i]);
-    }
-    return result;
+    return absolutes.reduce((acc, val, i) => acc + (signs[i]?val:-val),0);
 }
