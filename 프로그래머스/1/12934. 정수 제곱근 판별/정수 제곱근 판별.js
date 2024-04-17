0a5df3af23d41cba82ab8633fe25a874e5763a6b
Jangeunhye <65762430+Jangeunhye@users.noreply.github.com>
2024-04-17 10:10:03 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,8 +1,5 @@
 function solution(n) {
-    if(n===1)
-        return (n+1)**2;
-    
-    for(let x = 2; x<Math.floor(n/2);x++){
+    for(let x = 1; x<=Math.sqrt(n);x++){
         if(x**2===n)
             return (x+1)**2;
     }
